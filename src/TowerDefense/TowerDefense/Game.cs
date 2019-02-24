ba7bca9292ba8357be0227e70623575c76a872a3
Wyatt Fleming <44535851+wyattf1995@users.noreply.github.com>
2019-02-23 17:39:33 -0800

    Set map paramets to 8x5 and declared an area variable that multiplies the Width and Height

--- src/TowerDefense/TowerDefense/Game.cs
+++ src/TowerDefense/TowerDefense/Game.cs
@@ -11,6 +11,11 @@ namespace TowerDefense
         public void Main()
         {
             Map map = new Map();
+
+            map.Width = 8;
+            map.Height = 5;
+
+            int area = map.Width * map.Height;
         }
     }
 }
