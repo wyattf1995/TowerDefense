5b77b22c53f4d1347dd895f5b7ccb6ef03cbf103
Wyatt Fleming <44535851+wyattf1995@users.noreply.github.com>
2019-02-25 12:08:54 -0800

    Added a method that returns a boolean to the Map class that returns whether or not the point is inside the bounds of the Map parameters

--- src/TowerDefense/TowerDefense/Map.cs
+++ src/TowerDefense/TowerDefense/Map.cs
@@ -16,5 +16,11 @@ namespace TowerDefense
             Width = width;
             Height = height;
         }
+
+        public bool OnMap(Point point)
+        {
+            bool inBounds = point.X >= 0 && point.X < Width && point.Y >= 0 && point.Y < Height;
+            return inBounds;
+        }
     }
 }
