e72d0ef4b16603819df88243c6e973841a66f7a0
Wyatt Fleming <44535851+wyattf1995@users.noreply.github.com>
2019-02-25 11:59:46 -0800

    Added an empty constructor for the MapLocation class that passes the Point class constructor parameters using the base class

--- src/TowerDefense/TowerDefense/MapLocation.cs
+++ src/TowerDefense/TowerDefense/MapLocation.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TowerDefense
 {
-    class MapLocation
+    class MapLocation : Point
     {
+        public MapLocation(int x, int y) : base(x, y)
+        {
+
+        }
     }
 }
