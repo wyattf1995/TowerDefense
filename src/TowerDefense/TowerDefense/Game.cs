79c8283d8ee6ce4e69a23936d1569dd165cc5743
Wyatt Fleming <44535851+wyattf1995@users.noreply.github.com>
2019-02-25 18:28:31 -0800

    Added try-catch logic to the main method that will prevent a location on the map outside the range from existing

--- src/TowerDefense/TowerDefense/Game.cs
+++ src/TowerDefense/TowerDefense/Game.cs
@@ -12,8 +12,17 @@ namespace TowerDefense
         {
             Map map = new Map(8, 5);
 
-           
-           
+            try
+            {
+                MapLocation mapLocation = new MapLocation(20, 20, map);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("That point is not on the map!");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
