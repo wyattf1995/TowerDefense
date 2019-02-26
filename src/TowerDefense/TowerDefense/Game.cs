6742d1e9e09ec7a77180410608678cda980298bb
Wyatt Fleming <44535851+wyattf1995@users.noreply.github.com>
2019-02-25 18:20:20 -0800

    Added Map map parameter to the MapLocation method so as to block an object coming into existence with a point outside the range of the map

--- src/TowerDefense/TowerDefense/Game.cs
+++ src/TowerDefense/TowerDefense/Game.cs
@@ -12,17 +12,8 @@ namespace TowerDefense
         {
             Map map = new Map(8, 5);
 
-            Point point = new Point(4, 2);
-
-            bool isOnMap = map.OnMap(point);
-
-            Console.WriteLine(isOnMap);
-
-            point = new Point(8, 5);
-            isOnMap = map.OnMap(point);
-
-            Console.WriteLine(isOnMap);
-            Console.ReadKey();
+           
+           
         }
     }
 }
