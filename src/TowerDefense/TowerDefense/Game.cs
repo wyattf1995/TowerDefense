3ff1e54c7f6471d873cf842fa248e1d9b2826bf9
Wyatt Fleming <44535851+wyattf1995@users.noreply.github.com>
2019-02-26 17:31:06 -0800

    Moved the MapLocation array directly into the parameter of the Path class

--- src/TowerDefense/TowerDefense/Game.cs
+++ src/TowerDefense/TowerDefense/Game.cs
@@ -14,17 +14,19 @@ namespace TowerDefense
 
             try
             {
-                MapLocation[] path =
-                {
-                    new MapLocation(0, 2, map),
-                    new MapLocation(1, 2, map),
-                    new MapLocation(2, 2, map),
-                    new MapLocation(3, 2, map),
-                    new MapLocation(4, 2, map),
-                    new MapLocation(5, 2, map),
-                    new MapLocation(6, 2, map),
-                    new MapLocation(7, 2, map),
-                };
+                Path path = new Path(
+                    new []{
+                        new MapLocation(0, 2, map),
+                        new MapLocation(1, 2, map),
+                        new MapLocation(2, 2, map),
+                        new MapLocation(3, 2, map),
+                        new MapLocation(4, 2, map),
+                        new MapLocation(5, 2, map),
+                        new MapLocation(6, 2, map),
+                        new MapLocation(7, 2, map),
+                        }
+                    );
+               
             }
             catch (OutOfBoundsException e)
             {
