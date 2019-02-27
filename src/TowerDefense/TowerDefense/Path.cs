688abc6ba78080566815718f115d075b67ece228
Wyatt Fleming <44535851+wyattf1995@users.noreply.github.com>
2019-02-26 17:08:09 -0800

    Added a constructor for the Path class

--- src/TowerDefense/TowerDefense/Path.cs
+++ src/TowerDefense/TowerDefense/Path.cs
@@ -8,11 +8,11 @@ namespace TowerDefense
 {
     class Path
     {
-        private MapLocation[] _path;
+        private readonly MapLocation[] _path;
 
         public Path(MapLocation[] path)
         {
-
+            _path = path;
         }
     }
 }
