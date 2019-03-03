9557bc7073ff0f2e7093a04ce944f4935101417c
Wyatt Fleming <44535851+wyattf1995@users.noreply.github.com>
2019-03-03 14:52:09 -0800

    Created a MapLocation property that Gets and Sets rather than two seperate methods for doing so

--- src/TowerDefense/TowerDefense/Invader.cs
+++ src/TowerDefense/TowerDefense/Invader.cs
@@ -12,12 +12,24 @@ namespace TowerDefense
 
         public MapLocation GetLocation()
         {
-            return _location;
+            
         }
 
         public void SetLocation(MapLocation value)
         {
-            _location = value;
+            
+        }
+
+        public MapLocation Location
+        {
+            get
+            {
+                return _location;
+            }
+            set
+            {
+                _location = value;
+            }
         }
     }
 }
